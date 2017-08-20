edd6326c82ed299faa5c65c10211a77bc6a9c871
hzfangwentian <hzfangwentian@corp.netease.com>
2017-08-20 21:28:22 +0800

    init

---
+++ app.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const koa = require('koa');
+const config = require('config');
+const tplSetting = require('./lib/tplSetting.js');
+const render = require('./lib/render.js');
+const router = require('./lib/router.js');
+const app = new koa();
+const port = config.get('port');
+
+// koa-ejs参数设置
+tplSetting(app, __dirname);
+
+// 模板渲染
+app.use(render);
+
+// 路由配置
+app.use(router);
+
+app.use(async ctx => {
+    ctx.body = 'Hello world!';
+})
+
+app.listen(port);
