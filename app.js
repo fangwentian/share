df646823e684568660bfc830872a21098d2c3b62
hzfangwentian <hzfangwentian@corp.netease.com>
2017-08-21 23:23:37 +0800

    update

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const Koa = require('koa');
 const config = require('config');
+const serve = require('koa-static');
 const tplSetting = require('./lib/tplSetting.js');
 const render = require('./lib/render.js');
 const router = require('./lib/router.js');
@@ -10,6 +11,9 @@ const port = config.get('port');
 
 // koa-ejs参数设置
 tplSetting(app, __dirname);
+
+// 静态资源
+app.use(serve(path.join(__dirname, '/dist')));
 
 // 模板渲染
 app.use(render);
