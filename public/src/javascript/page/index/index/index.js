3f1b41fcbe11d698ca1485b0ea7537be0c6157aa
fangwentian <654396466@qq.com>
2017-08-27 01:57:47 +0800

    ui

--- public/src/javascript/page/index/index/index.js
+++ public/src/javascript/page/index/index/index.js
@@ -1,9 +1,12 @@
-// 页面入口，路由配置
 import 'font-awesome-webpack';
 import 'src/less/base.less';
 import Vue from 'vue';
 import router from 'src/javascript/router/router';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-default/index.css';
 // import store from 'src/javascript/store';
+
+Vue.use(ElementUI);
 
 new Vue({
     delimiters: ['{', '}'],
