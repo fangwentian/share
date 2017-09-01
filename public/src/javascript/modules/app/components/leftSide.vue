2a217dd9dcfcb90193e7939f38d9fc430162832b
hzfangwentian <hzfangwentian@corp.netease.com>
2017-09-01 16:41:01 +0800

    'update'

--- public/src/javascript/modules/app/components/leftSide.vue
+++ public/src/javascript/modules/app/components/leftSide.vue
@@ -1,10 +1,10 @@
 <template>
     <div>
         <div class="f-pr">
-            <a href="javascript:;" class="new">新建</a>
-            <ul class="operations">
-                <li><i class="fa fa-upload icon" aria-hidden="true"></i>导入文件</li>
-                <li><i class="fa fa-folder icon" aria-hidden="true"></i>文件夹</li>
+            <a href="javascript:;" class="new" @click.stop="showOperations">新建</a>
+            <ul class="operations" v-if="isShowOperations">
+                <li><i class="fa fa-upload" aria-hidden="true"></i>导入文件</li>
+                <li><i class="fa fa-folder" aria-hidden="true"></i>文件夹</li>
             </ul>
         </div>
         <ul class="classification">
@@ -14,68 +14,76 @@
         </ul>
     </div>
 </template>
+
 <script>
 export default {
+    data() {
+        return {
+            isShowOperations: false
+        }
+    },
+    created() {
+        document.onclick = () => {
+            this.isShowOperations = false;
+        }
+    },
     methods: {
-        handleOpen(key, keyPath) {
-            console.log(key, keyPath);
-        },
-        handleClose(key, keyPath) {
-            console.log(key, keyPath);
+        showOperations() {
+            this.isShowOperations = !this.isShowOperations;
         }
     }
 }
 </script>
 
 <style scoped lang="less">
-.new {
-    width: 80px;
-    display: block;
-    height: 28px;
-    background-color: #41464b;
-    color: #fff;
-    text-align: center;
-    line-height: 28px;
-}
-.operations {
-    display: none;
-    width: 135px;
-    margin-top: 5px;
-    border: solid 1px #ddd;
-    padding: 13px 0 13px 0;
-    box-shadow: 0 4px 12px 0 rgba(0,0,0,.1);
-    position: absolute;
-    background: #fff;
-    li {
-        height: 36px;
-        line-height: 36px;
-        cursor: pointer;
-        padding: 0 12px 0 20px;
-        &:hover {
-            background-color: #f5f5f5;
+    .new {
+        width: 80px;
+        display: block;
+        height: 28px;
+        background-color: #41464b;
+        color: #fff;
+        text-align: center;
+        line-height: 28px;
+    }
+    .operations {
+        width: 135px;
+        margin-top: 5px;
+        border: solid 1px #ddd;
+        padding: 13px 0 13px 0;
+        box-shadow: 0 4px 12px 0 rgba(0,0,0,.1);
+        position: absolute;
+        background: #fff;
+        z-index: 1;
+        li {
+            height: 36px;
+            line-height: 36px;
+            cursor: pointer;
+            padding: 0 12px 0 20px;
+            &:hover {
+                background-color: #f5f5f5;
+            }
+            i {
+                margin-right: 12px;
+                color: #93a2b1;
+                font-size: 16px;
+                vertical-align: middle;
+                position: relative;
+                top: -1px;
+            }
+        }
+    }
+    .classification {
+        margin-top: 30px;
+        li {
+            height: 30px;
+            margin-bottom: 10px;
         }
         i {
-            margin-right: 12px;
+            margin-right: 13px;
+            font-size: 16px;
+            vertical-align: middle;
+            position: relative;
+            top: -1px;
         }
     }
-    .icon {
-        color: #93a2b1;
-        font-size: 16px;
-        vertical-align: middle;
-        position: relative;
-        top: -1px;
-    }
-}
-.classification {
-    margin-top: 30px; 
-    li {
-        height: 30px;
-        margin-bottom: 10px;
-    }
-    i {
-        margin-right: 15px;
-    }
-}
-
-
 </style>
